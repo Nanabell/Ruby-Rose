cd62ee0cbba1e39f44fbf6cbe38d0c1b2b51d6ff
Nanabell <marlon.delfin@googlemail.com>
2017-03-23 23:51:03 +0100

    added Hi Command

---
+++ Ruby Rose/Modules/Rwby/HiCommand.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.Commands;
+using RubyRose.Common.Preconditions;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace RubyRose.Modules.Rwby
+{
+    [Name("RWBY")]
+    public class HiCommand : ModuleBase
+    {
+        [Command("Hi")]
+        [RequireAllowed, Ratelimit(5, 30, Measure.Seconds)]
+        [RequireBotPermission(ChannelPermission.SendMessages)]
+        public async Task Hi()
+        {
+            var direc = new DirectoryInfo(Assembly.GetEntryAssembly().Location);
+            do
+            {
+                direc = direc.Parent;
+            }
+            while (direc.Name != "Ruby Rose");
+            await Context.Channel.SendFileAsync($"{direc.FullName}/Data/Hi.jpg");
+        }
+    }
+}
