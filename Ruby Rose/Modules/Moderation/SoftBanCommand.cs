150a26dd9df1f3f10894eca567bfe72c51261a41
Nanabell <marlon.delfin@googlemail.com>
2017-03-24 16:16:41 +0100

    added softban command

---
+++ Ruby Rose/Modules/Moderation/SoftBanCommand.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.Commands;
+using RubyRose.Common;
+using RubyRose.Common.Preconditions;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RubyRose.Modules.Moderation
+{
+    [Name("Moderation")]
+    public class SoftBanCommand : ModuleBase
+    {
+        [Command("SoftBan")]
+        [MinPermission(AccessLevel.ServerModerator)]
+        [RequireBotPermission(GuildPermission.BanMembers), RequireUserPermission(GuildPermission.BanMembers)]
+        public async Task SoftBan(IGuildUser user, int prunedays = 7)
+        {
+            var invokerpos = getPosition((Context.User as IGuildUser));
+            var targetpos = getPosition(user);
+            var botpos = getPosition(await Context.Guild.GetCurrentUserAsync());
+            if (botpos > targetpos)
+            {
+                if (invokerpos > targetpos || Context.User.Id == Context.Guild.OwnerId)
+                {
+                    await Context.Guild.AddBanAsync(user, prunedays);
+                    await Context.Guild.RemoveBanAsync(user);
+                    await Context.Channel.SendEmbedAsync(Embeds.Success("*Softbanned!*", $"{user} has been softbanned from the Guild!"));
+                }
+                else await Context.Channel.SendEmbedAsync(Embeds.UnmetPrecondition($"You cant softban someone who is above or equal to you in the role hierarchy"));
+            }
+            else await Context.Channel.SendEmbedAsync(Embeds.UnmetPrecondition("I cant softban somone who is higher or equal to me in the role hierarchy"));
+        }
+
+        private int getPosition(IGuildUser user)
+            => user.GetRoles().OrderByDescending(x => x.Position).FirstOrDefault().Position;
+    }
+}
