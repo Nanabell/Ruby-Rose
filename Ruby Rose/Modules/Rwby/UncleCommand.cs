b45eb009a8e89c6fe2224e38bc7d75c3d4c9687f
Nanabell <marlon.delfin@googlemail.com>
2017-03-24 07:04:01 +0100

    Uncle Command

---
+++ Ruby Rose/Modules/Rwby/UncleCommand.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.Commands;
+using RubyRose.Common.Preconditions;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace RubyRose.Modules.Rwby
+{
+    [Name("RWBY")]
+    public class UncleCommand : ModuleBase
+    {
+        [Command("Uncle")]
+        [RequireAllowed, Ratelimit(1, 30, Measure.Seconds)]
+        [RequireBotPermission(ChannelPermission.AttachFiles)]
+        public async Task Uncle()
+        {
+            var direc = new DirectoryInfo(Assembly.GetEntryAssembly().Location);
+            do
+            {
+                direc = direc.Parent;
+            }
+            while (direc.Name != "Ruby Rose");
+            await Context.Channel.SendFileAsync($"{direc.FullName}/Data/uncle.gif");
+        }
+    }
+}
