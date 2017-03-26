157e0e220960fbbc6c447d5d0836fea53aedb833
Nanabell <marlon.delfin@googlemail.com>
2017-03-26 12:02:03 +0200

    Added Check for Special Permission

--- Ruby Rose/Common/Preconditions/MinPermissionAttribute.cs
+++ Ruby Rose/Common/Preconditions/MinPermissionAttribute.cs
@@ -2,6 +2,9 @@
 using System.Threading.Tasks;
 using Discord.Commands;
 using Discord.WebSocket;
+using RubyRose.Database;
+using System.Linq;
+using MongoDB.Driver;
 
 namespace RubyRose.Common.Preconditions
 {
@@ -10,15 +13,18 @@ namespace RubyRose.Common.Preconditions
         Blocked,
         IsPrivate,
         User,
-        Trusted,
         ServerModerator,
         ServerOwner,
+        Special,
         BotOwner
     }
+
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class MinPermissionAttribute : PreconditionAttribute
     {
         private readonly AccessLevel _level;
+        private static MongoClient _mongo;
+
         public MinPermissionAttribute(AccessLevel level)
         {
             _level = level;
@@ -26,11 +32,17 @@ namespace RubyRose.Common.Preconditions
 
         public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IDependencyMap map)
         {
-            var access = GetPermissions(context);
-            return Task.FromResult(access >= _level ? PreconditionResult.FromSuccess() : PreconditionResult.FromError("Not Enough Permissions!"));
+            _mongo = map.Get<MongoClient>();
+            var access = GetPermissions(_mongo, context, _level);
+
+            if (_level == AccessLevel.Special)
+            {
+                return Task.FromResult(access >= _level ? PreconditionResult.FromSuccess() : PreconditionResult.FromError("This command needs special permissions!"));
+            }
+            return Task.FromResult(access >= _level ? PreconditionResult.FromSuccess() : PreconditionResult.FromError("Not enough permissions!"));
         }
 
-        public AccessLevel GetPermissions(ICommandContext context)
+        public AccessLevel GetPermissions(MongoClient mongo, ICommandContext context, AccessLevel requestedLvl)
         {
             if (context.User.IsBot) return AccessLevel.Blocked;
 
@@ -40,9 +52,22 @@ namespace RubyRose.Common.Preconditions
 
             var user = context.User as SocketGuildUser;
             if (user == null) return AccessLevel.IsPrivate;
+
+            if (requestedLvl == AccessLevel.Special)
+            {
+                var c = mongo.GetDiscordDb<DatabaseModel>("Ruby Rose");
+                var cGuild = c.Find(x => x.Id == context.Guild.Id).First();
+
+                if (cGuild.User.Any(u => u.Id == user.Id))
+                {
+                    var cUser = cGuild.User.First(u => u.Id == user.Id);
+                    if (cUser.IsSpecial)
+                        return AccessLevel.Special;
+                }
+            }
+
             if (context.Guild.OwnerId == user.Id) return AccessLevel.ServerOwner;
             if (user.GuildPermissions.BanMembers || user.GuildPermissions.KickMembers) return AccessLevel.ServerModerator;
-            //TRUSTED
             return AccessLevel.User;
         }
     }
