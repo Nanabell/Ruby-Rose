b6f4955b6fbb6de8af65ac629eb03e6bff8d402a
Nanabell <marlon.delfin@googlemail.com>
2017-03-26 19:41:16 +0200

    Added Sleep command

---
+++ Ruby Rose/Modules/Fun/SleepCommand.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.Commands;
+using RubyRose.Common.Preconditions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubyRose.Modules.Fun
+{
+    [Name("Fun")]
+    public class SleepCommand : ModuleBase
+    {
+        [Command("Sleep")]
+        [RequireAllowed, Ratelimit(1, 30, Measure.Seconds)]
+        [RequireBotPermission(ChannelPermission.AttachFiles)]
+        public async Task Sleep()
+        {
+            var direc = new DirectoryInfo(Assembly.GetEntryAssembly().Location);
+            do
+            {
+                direc = direc.Parent;
+            }
+            while (direc.Name != "Ruby Rose");
+            await Context.Channel.SendFileAsync($"{direc.FullName}/Data/sleep.png");
+        }
+    }
+}
