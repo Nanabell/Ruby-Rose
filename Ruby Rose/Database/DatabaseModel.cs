edb1dbd24c6cb346d6ebbf87b9656fa5af2c48f0
Nanabell <marlon.delfin@googlemail.com>
2017-03-26 12:01:28 +0200

    Added new Database Model

---
+++ Ruby Rose/Database/DatabaseModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace RubyRose.Database
+{
+    public class DatabaseModel
+    {
+        public ulong Id { get; set; }
+
+        [BsonIgnoreIfNull]
+        public List<Users> User { get; set; }
+
+        [BsonIgnoreIfNull]
+        public List<Tags> Tag { get; set; }
+
+        [BsonIgnoreIfNull]
+        public List<Commands> Command { get; set; }
+
+        [BsonIgnoreIfNull]
+        public Roles OneTruePair { get; set; }
+    }
+
+    public class Users
+    {
+        public ulong Id { get; set; }
+        public bool IsSpecial { get; set; }
+        public uint Money { get; set; }
+    }
+
+    public class Tags
+    {
+        public string Name { get; set; }
+        public int Uses { get; set; }
+        public ulong CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUsed { get; set; }
+        public string Response { get; set; }
+    }
+
+    public class Commands
+    {
+        public Whitelists Whitelist { get; set; }
+        public Blacklists Blacklist { get; set; }
+
+        public class Whitelists
+        {
+            [BsonIgnoreIfNull]
+            public List<Users> User { get; set; }
+
+            [BsonIgnoreIfNull]
+            public List<Channels> Channel { get; set; }
+
+            public class Users
+            {
+                public ulong Id { get; set; }
+            }
+
+            public class Channels
+            {
+                public ulong Id { get; set; }
+            }
+        }
+
+        public class Blacklists
+        {
+            [BsonIgnoreIfNull]
+            public List<Users> User { get; set; }
+
+            [BsonIgnoreIfNull]
+            public List<Channels> Channel { get; set; }
+
+            public class Users
+            {
+                public ulong Id { get; set; }
+            }
+
+            public class Channels
+            {
+                public ulong Id { get; set; }
+            }
+        }
+    }
+
+    public class Roles
+    {
+        public ulong Id { get; set; }
+    }
+}
