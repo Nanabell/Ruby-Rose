5b087d703198298c1883c0ef81e3a0f6a7170c63
Nanabell <marlon.delfin@googlemail.com>
2017-03-26 12:01:44 +0200

    Added Extention Method for new Database Model

---
+++ Ruby Rose/Common/DBextentions.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RubyRose.Common
+{
+    public static class DBextentions
+    {
+        public static IMongoCollection<T> GetDiscordDb<T>(this MongoClient mongo, string name)
+        {
+            var db = mongo.GetDatabase("Discord");
+            var collection = db.GetCollection<T>(name);
+            return collection;
+        }
+    }
+}
