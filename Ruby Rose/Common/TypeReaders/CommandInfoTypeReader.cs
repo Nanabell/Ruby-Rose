3ef629b322404308dd627c9b8ee3cecd553ee163
Nanabell <marlon.delfin@googlemail.com>
2017-04-02 16:46:39 +0200

    Added CommandInfo TypeReader

---
+++ Ruby Rose/Common/TypeReaders/CommandInfoTypeReader.cs
@@ -0,0 +1,31 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubyRose.Common.TypeReaders
+{
+    internal class CommandInfoTypeReader : TypeReader
+    {
+        private readonly CommandService _service;
+
+        public CommandInfoTypeReader(CommandService service)
+        {
+            _service = service;
+        }
+
+        public override Task<TypeReaderResult> Read(ICommandContext context, string input)
+        {
+            var result = _service.Search(context, input);
+
+            if (result.IsSuccess)
+            {
+                return Task.FromResult(TypeReaderResult.FromSuccess(result.Commands.FirstOrDefault().Command));
+            }
+            else
+                return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed, $"Command {input} not found"));
+        }
+    }
+}
