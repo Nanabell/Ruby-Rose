3a1ab55d792ae01a5c222251fc1f023b984d8c9a
Nanabell <marlon.delfin@googlemail.com>
2017-04-09 14:12:25 +0200

    Added Service Base & Handler

---
+++ Ruby Rose/Services/ServiceBase.cs
@@ -0,0 +1,54 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubyRose.Services
+{
+    public abstract class ServiceBase
+    {
+        public static DiscordSocketClient Client;
+        public static IDependencyMap Map;
+        internal static Logger _logger = LogManager.GetCurrentClassLogger();
+        public bool IsEnabled { get; internal set; }
+
+        protected abstract Task PreEnable();
+
+        protected abstract Task PreDisable();
+
+        protected abstract bool WaitForReady();
+
+        public async Task<bool> TryPreEnable()
+        {
+            if (WaitForReady())
+                return false;
+            else
+                return await TryEnable();
+        }
+
+        public async Task<bool> TryEnable()
+        {
+            if (IsEnabled)
+                return false;
+
+            await PreEnable();
+            IsEnabled = true;
+            _logger.Info($"Enabled Service {GetType().Name}");
+            return true;
+        }
+
+        public async Task<bool> TryDisable()
+        {
+            if (!IsEnabled)
+                return false;
+
+            await PreDisable();
+            IsEnabled = false;
+            _logger.Info($"Disabled Service {GetType().Name}");
+            return true;
+        }
+    }
+}
---
+++ Ruby Rose/Services/ServiceHandler.cs
@@ -0,0 +1,67 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubyRose.Services
+{
+    public class ServiceHandler
+    {
+        private List<ServiceBase> _registeredServices = new List<ServiceBase>();
+        private List<ServiceBase> _lateRegister = new List<ServiceBase>();
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+        private readonly DiscordSocketClient _client;
+
+        public ServiceHandler(IDependencyMap map)
+        {
+            _client = map.Get<DiscordSocketClient>();
+            ServiceBase.Client = _client;
+            ServiceBase.Map = map;
+
+            var allInstantServices = Assembly.GetEntryAssembly().ExportedTypes
+                .Where(x => x.GetTypeInfo().BaseType == typeof(ServiceBase));
+
+            foreach (var s in allInstantServices)
+            {
+                var constructor = s.GetConstructors().FirstOrDefault(c => c.GetParameters().Count() == 0);
+                var service = (ServiceBase)constructor.Invoke(new object[0]);
+
+                if (service.TryPreEnable().GetAwaiter().GetResult())
+                {
+                    _registeredServices.Add(service);
+                    _logger.Info($"Registered and Enabled Service {s.Name}");
+                }
+                else
+                {
+                    _logger.Info($"Registered Service {s.Name}");
+                    _registeredServices.Add(service);
+                    _lateRegister.Add(service);
+                }
+            }
+            if (_lateRegister.Count > 0)
+                _client.Ready += LateRegister;
+        }
+
+        private async Task LateRegister()
+        {
+            foreach (var service in _lateRegister)
+            {
+                await service.TryEnable();
+            }
+        }
+
+        public TService GetService<TService>() where TService : ServiceBase
+            => _registeredServices.FirstOrDefault(x => x.GetType() == typeof(TService)) as TService;
+
+        public async Task<bool> TryEnable<TService>() where TService : ServiceBase
+            => await (GetService<TService>()).TryEnable();
+
+        public async Task<bool> TryDisable<TService>() where TService : ServiceBase
+            => await (GetService<TService>()).TryDisable();
+    }
+}
