35426b721388e4afe1106d2cac4c3073f5a27dcc
Nanabell <marlon.delfin@googlemail.com>
2017-04-09 14:13:02 +0200

    Reworked Bot - Start + Load Services

--- Ruby Rose/Program.cs
+++ Ruby Rose/Program.cs
@@ -4,12 +4,12 @@ using Discord.WebSocket;
 using MongoDB.Driver;
 using NLog;
 using RubyRose.Common;
-using RubyRose.Common.Handler;
+using RubyRose.Services;
 using System.Threading.Tasks;
 
 namespace RubyRose
 {
-    internal class Program
+    public class Program
     {
         private static Logger logger = LogManager.GetCurrentClassLogger();
 
@@ -17,45 +17,108 @@ namespace RubyRose
 
         public async Task Start()
         {
-            Utils.LoadNLogSettings();
+            logger.Info("\n" + @"
+                                      bbbbbbbb
+RRRRRRRRRRRRRRRRR                     b::::::b                                          RRRRRRRRRRRRRRRRR
+R::::::::::::::::R                    b::::::b                                          R::::::::::::::::R
+R::::::RRRRRR:::::R                   b::::::b                                          R::::::RRRRRR:::::R
+RR:::::R     R:::::R                   b:::::b                                          RR:::::R     R:::::R
+  R::::R     R:::::Ruuuuuu    uuuuuu   b:::::bbbbbbbbb    yyyyyyy           yyyyyyy       R::::R     R:::::R   ooooooooooo       ssssssssss       eeeeeeeeeeee
+  R::::R     R:::::Ru::::u    u::::u   b::::::::::::::bb   y:::::y         y:::::y        R::::R     R:::::R oo:::::::::::oo   ss::::::::::s    ee::::::::::::ee
+  R::::RRRRRR:::::R u::::u    u::::u   b::::::::::::::::b   y:::::y       y:::::y         R::::RRRRRR:::::R o:::::::::::::::oss:::::::::::::s  e::::::eeeee:::::ee
+  R:::::::::::::RR  u::::u    u::::u   b:::::bbbbb:::::::b   y:::::y     y:::::y          R:::::::::::::RR  o:::::ooooo:::::os::::::ssss:::::se::::::e     e:::::e
+  R::::RRRRRR:::::R u::::u    u::::u   b:::::b    b::::::b    y:::::y   y:::::y           R::::RRRRRR:::::R o::::o     o::::o s:::::s  ssssss e:::::::eeeee::::::e
+  R::::R     R:::::Ru::::u    u::::u   b:::::b     b:::::b     y:::::y y:::::y            R::::R     R:::::Ro::::o     o::::o   s::::::s      e:::::::::::::::::e
+  R::::R     R:::::Ru::::u    u::::u   b:::::b     b:::::b      y:::::y:::::y             R::::R     R:::::Ro::::o     o::::o      s::::::s   e::::::eeeeeeeeeee
+  R::::R     R:::::Ru:::::uuuu:::::u   b:::::b     b:::::b       y:::::::::y              R::::R     R:::::Ro::::o     o::::ossssss   s:::::s e:::::::e
+RR:::::R     R:::::Ru:::::::::::::::uu b:::::bbbbbb::::::b        y:::::::y             RR:::::R     R:::::Ro:::::ooooo:::::os:::::ssss::::::se::::::::e
+R::::::R     R:::::R u:::::::::::::::u b::::::::::::::::b          y:::::y              R::::::R     R:::::Ro:::::::::::::::os::::::::::::::s  e::::::::eeeeeeee
+R::::::R     R:::::R  uu::::::::uu:::u b:::::::::::::::b          y:::::y               R::::::R     R:::::R oo:::::::::::oo  s:::::::::::ss    ee:::::::::::::e
+RRRRRRRR     RRRRRRR    uuuuuuuu  uuuu bbbbbbbbbbbbbbbb          y:::::y                RRRRRRRR     RRRRRRR   ooooooooooo     sssssssssss        eeeeeeeeeeeeee
+                                                                y:::::y
+                                                               y:::::y
+                                                              y:::::y
+                                                             y:::::y
+                                                            yyyyyyy");
 
-            logger.Debug("[Configuration] Loading configuration");
-            var config = Utils.LoadConfig();
+            logger.Info("Loading configuration");
+            var config = CoreConfig.ReadConfig();
 
-            logger.Debug("[Discord] Initiating DiscordClient");
+            logger.Info("Initiating DiscordClient");
             var client = new DiscordSocketClient(new DiscordSocketConfig
             {
                 AlwaysDownloadUsers = true,
                 LogLevel = LogSeverity.Debug,
-                MessageCacheSize = 10
+                MessageCacheSize = 100
             });
 
-            logger.Debug("[Database] Connecting to MongoDb");
-            var mongo = new MongoClient(config.Database.Mongo);
-            var handler = new CommandHandler();
+            client.Log += logging;
 
-            logger.Debug("[Discord] Creating new DependencyMap");
+            logger.Info("Connecting to MongoDb");
+            var mongo = new MongoClient(config.Database.Mongo);
+
             var map = new DependencyMap();
             map.Add(client);
             map.Add(mongo);
             map.Add(config);
-            logger.Trace("[Discord] Added Client, Mongodb & config to DependencyMap");
 
-            logger.Debug("[EventHandler] Installing Event Handler");
-            var events = new EventHandlers(map);
-            await events.Install();
+            logger.Info("Initializing Service Handler");
+            new ServiceHandler(map);
 
-            await SettingsManager.Install(map);
+            logger.Info($"Starting Login to Discord");
+            await client.LoginAsync(TokenType.Bot, (config.IsMainBot ? config.Token.Main : config.Token.Dev));
 
-            logger.Trace($"[Gateway] Starting Login to Discord");
-            await client.LoginAsync(TokenType.Bot, (config.IsTestBot ? config.TestBotToken : config.Token));
-            logger.Info("[Gateway] Starting Bot");
+            logger.Info("Starting Bot");
             await client.StartAsync();
 
-            logger.Info("[CommandService] Installing Command Handler");
+            logger.Info("Initializing Command Handler");
+            var handler = new CommandHandler();
             await handler.Install(map);
 
             await Task.Delay(-1);
+        }
+
+        public static Task logging(LogMessage arg)
+        {
+            LogLevel level;
+
+            switch (arg.Severity)
+            {
+                case LogSeverity.Debug:
+                    level = LogLevel.Trace;
+                    break;
+
+                case LogSeverity.Verbose:
+                    level = LogLevel.Debug;
+                    break;
+
+                case LogSeverity.Info:
+                    level = LogLevel.Info;
+                    break;
+
+                case LogSeverity.Warning:
+                    level = LogLevel.Warn;
+                    break;
+
+                case LogSeverity.Error:
+                    level = LogLevel.Error;
+                    break;
+
+                case LogSeverity.Critical:
+                    level = LogLevel.Fatal;
+                    break;
+
+                default:
+                    level = LogLevel.Off;
+                    break;
+            }
+
+            if (arg.Exception == null)
+                logger.Log(level, arg.Message);
+            else
+                logger.Log(level, arg.Exception, arg.Message);
+
+            return Task.CompletedTask;
         }
     }
 }
