b56bee6ee0f257f6644f5e6cb0f804802654e7b5
Nanabell <marlon.delfin@googlemail.com>
2017-04-09 14:11:28 +0200

    Split Config Command to own Commands

---
+++ Ruby Rose/Modules/Moderation/ResultAnnnounceSettings.cs
@@ -0,0 +1,61 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using MongoDB.Driver;
+using RubyRose.Common.Preconditions;
+using RubyRose.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubyRose.Modules.Moderation
+{
+    [Name("Config")]
+    public class ResultAnnnounceSettings : ModuleBase
+    {
+        [Name("Result Announce"), Group("Result Announce")]
+        public class ResultAnnounceSettingsCommands : ModuleBase
+        {
+            private readonly MongoClient _mongo;
+
+            public ResultAnnounceSettingsCommands(IDependencyMap map)
+            {
+                _mongo = map.Get<MongoClient>();
+            }
+
+            [Command("Enable"), Alias("On, True, 1, Yes")]
+            [MinPermission(AccessLevel.ServerModerator)]
+            public async Task On()
+            {
+                var allsettings = _mongo.GetCollection<Settings>(Context.Client);
+                var settings = await allsettings.GetByGuildAsync(Context.Guild);
+
+                if (!settings.ResultAnnounce)
+                {
+                    settings.ResultAnnounce = true;
+                    await allsettings.SaveAsync(settings);
+                    await CommandHandler.ReloadResultAnnounce(Context.Client as DiscordSocketClient, _mongo);
+                    await Context.ReplyAsync("Result Announce has been Enabled!");
+                }
+                else await Context.ReplyAsync("Result Announce is already Enabled");
+            }
+
+            [Command("Disable"), Alias("Off, False, 0, No")]
+            [MinPermission(AccessLevel.ServerModerator)]
+            public async Task Off()
+            {
+                var allsettings = _mongo.GetCollection<Settings>(Context.Client);
+                var settings = await allsettings.GetByGuildAsync(Context.Guild);
+
+                if (settings.ResultAnnounce)
+                {
+                    settings.ResultAnnounce = false;
+                    await allsettings.SaveAsync(settings);
+                    await CommandHandler.ReloadResultAnnounce(Context.Client as DiscordSocketClient, _mongo);
+                    await Context.ReplyAsync("Result Announce has been Disabled!");
+                }
+                else await Context.ReplyAsync("Result Announce is already Disabled");
+            }
+        }
+    }
+}
---
+++ Ruby Rose/Modules/Moderation/RwbyFightSettings.cs
@@ -0,0 +1,60 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using MongoDB.Driver;
+using RubyRose.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubyRose.Modules.Moderation
+{
+    [Name("Config")]
+    public class RwbyFightSettings : ModuleBase
+    {
+        [Name("Rwby Fight"), Group("Rwby Fight")]
+        public class RwbyFightSettingsCommands : ModuleBase
+        {
+            private readonly MongoClient _mongo;
+
+            public RwbyFightSettingsCommands(IDependencyMap map)
+            {
+                _mongo = map.Get<MongoClient>();
+            }
+
+            [Command("Enable"), Alias("On, True, 1, Yes")]
+            [MinPermission(AccessLevel.ServerModerator)]
+            public async Task On()
+            {
+                var allsettings = _mongo.GetCollection<Settings>(Context.Client);
+                var settings = await allsettings.GetByGuildAsync(Context.Guild);
+
+                if (!settings.RwbyFight)
+                {
+                    settings.RwbyFight = true;
+                    await allsettings.SaveAsync(settings);
+                    await CommandHandler.ReloadResultAnnounce(Context.Client as DiscordSocketClient, _mongo);
+                    await Context.ReplyAsync("Rwby Fight has been Enabled!");
+                }
+                else await Context.ReplyAsync("Rwby Fight is already Enabled");
+            }
+
+            [Command("Disable"), Alias("Off, False, 0, No")]
+            [MinPermission(AccessLevel.ServerModerator)]
+            public async Task Off()
+            {
+                var allsettings = _mongo.GetCollection<Settings>(Context.Client);
+                var settings = await allsettings.GetByGuildAsync(Context.Guild);
+
+                if (settings.RwbyFight)
+                {
+                    settings.RwbyFight = false;
+                    await allsettings.SaveAsync(settings);
+                    await CommandHandler.ReloadResultAnnounce(Context.Client as DiscordSocketClient, _mongo);
+                    await Context.ReplyAsync("Rwby Fight has been Disabled!");
+                }
+                else await Context.ReplyAsync("Rwby Fight is already Disabled");
+            }
+        }
+    }
+}
