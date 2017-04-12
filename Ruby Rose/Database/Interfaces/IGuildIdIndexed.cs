9bcef53c4f72779a65555dc82d96ed6f63a13440
Nanabell <marlon.delfin@googlemail.com>
2017-04-12 05:47:21 +0200

    Added IGuildIdIndexed Interface

---
+++ Ruby Rose/Database/Interfaces/IGuildIdIndexed.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RubyRose.Database
+{
+    public interface IGuildIdIndexed : IGuildIndexed
+    {
+        ulong MessageId { get; set; }
+    }
+}
