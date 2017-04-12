ee7e1586e4e7501e1a560fda58b225833b9b0f65
Nanabell <marlon.delfin@googlemail.com>
2017-04-12 16:56:25 +0200

    Improved CommandHandler

--- Ruby Rose/CommandHandler.cs
+++ Ruby Rose/CommandHandler.cs
@@ -27,17 +27,24 @@ namespace RubyRose
         {
             var argPos = 0;
             var message = parameterMessage as SocketUserMessage;
-            if (message == null) return;
-            if (message.Content == _config.Prefix) return;
-
             var author = message.Author as SocketGuildUser;
-            if (author == null) return;
-            if (!(message.HasMentionPrefix(_client.CurrentUser, ref argPos) || message.HasStringPrefix(_config.Prefix, ref argPos))) return;
 
             var context = new CommandContext(_client, message);
-            var searchResult = _commandService.Search(context, argPos);
+
+            if (message == null)
+                return;
+            if (message.Content == _config.Prefix)
+                return;
+            if (author == null)
+                return;
+            if (!(message.HasMentionPrefix(_client.CurrentUser, ref argPos) || message.HasStringPrefix(_config.Prefix, ref argPos)))
+                return;
+            if (!context.Channel.CheckChannelPermission(ChannelPermission.SendMessages, await context.Guild.GetCurrentUserAsync()))
+                return;
+
+            var sResult = _commandService.Search(context, argPos);
             CommandInfo commandInfo = null;
-            if (searchResult.IsSuccess) commandInfo = searchResult.Commands.First().Command;
+            if (sResult.IsSuccess) commandInfo = sResult.Commands.First().Command;
 
             await Task.Run(async () =>
             {
@@ -45,40 +52,36 @@ namespace RubyRose
                 {
                     var result = await _commandService.ExecuteAsync(context, argPos, _map);
 
+                    logger.Info($"Command ran by {context.User} in {context.Guild.Name} - {context.Message.Content}");
+
                     if (!result.IsSuccess)
                     {
-                        logger.Warn($"{GetCommandName(commandInfo, message, argPos)}" +
-                            $" [{(context.IsPrivate ? "Private" : context.Guild.Name)}]" +
-                            $" {(context.IsPrivate ? "" : $"#{context.Channel.Name} ")}" +
-                            $"({context.User})" +
-                            $" {message.Content.Substring(GetCommandName(commandInfo, message, argPos).Length + _config.Prefix.Length)}" +
-                            $" => {result.Error}");
+                        logger.Warn($"Command failed to run successfully. {result.ErrorReason}");
 
                         ResultAnnounce.TryGetValue(context.Guild.Id, out var IsEnabled);
                         if (IsEnabled)
                         {
-                            if (result is ExecuteResult)
+                            string response = null;
+                            switch (result)
                             {
-                                ExecuteError(context, (ExecuteResult)result, searchResult);
-                            }
-                            else if (result is PreconditionResult)
-                            {
-                                await context.Channel.SendEmbedAsync(Embeds.UnmetPrecondition(((PreconditionResult)result).ErrorReason));
-                            }
-                            else if (result is ParseResult)
-                            {
-                                await context.Channel.SendEmbedAsync(Embeds.Invalid(((ParseResult)result).ErrorReason));
+                                case SearchResult searchResult:
+                                    break;
+
+                                case ParseResult parseResult:
+                                    response = $":warning: There was an error parsing your command: `{parseResult.ErrorReason}`";
+                                    break;
+
+                                case PreconditionResult preconditionResult:
+                                    response = $":warning: A precondition of your command failed: `{preconditionResult.ErrorReason}`";
+                                    break;
+
+                                case ExecuteResult executeResult:
+                                    response = $":warning: Your command failed to execute. If this persists, contact the Bot Developer.\n`{executeResult.Exception.Message}`";
+                                    logger.Error(executeResult.Exception);
+                                    break;
                             }
                         }
                         else logger.Warn($"Suppressing Result on behalf of settings");
-                    }
-                    else
-                    {
-                        logger.Info($"{GetCommandName(commandInfo, message, argPos)}" +
-                            $" [{(context.IsPrivate ? "Private" : context.Guild.Name)}]" +
-                            $" {(context.IsPrivate ? "" : $"#{context.Channel.Name} ")}" +
-                            $"({context.User})" +
-                            $" {message.Content.Substring(GetCommandName(commandInfo, message, argPos).Length + _config.Prefix.Length)}");
                     }
                 }
                 catch (Exception e)
@@ -130,30 +133,6 @@ namespace RubyRose
             logger.Info("Starting CommandHandler");
             _commandService.Log += Program.Logging;
             _client.MessageReceived += HandleCommand;
-        }
-
-        private static async void ExecuteError(ICommandContext context, ExecuteResult executeResult, SearchResult searchResult)
-        {
-            logger.Error(executeResult.Exception, $"An Error Occured on {context.Guild.Name} caused by {context.User} Exeption:");
-            var embed = new EmbedBuilder
-            {
-                Title = "Error executing command",
-                Description = string.Format("User {0} failed to execute command **{1}**.\n\nError Reason:\n{2}", context.User,
-                    searchResult.Commands.FirstOrDefault().Command.Name, executeResult.ErrorReason.LimitLengh(1000)),
-                Color = new Color(255, 127, 0),
-                ThumbnailUrl = context.User.GetAvatarUrl(),
-
-                Footer = new EmbedFooterBuilder
-                {
-                    Text = "Please Report this to the BotOwner!"
-                }
-            };
-
-            try
-            {
-                await context.Channel.SendEmbedAsync(embed);
-            }
-            catch { logger.Warn("Not enough Permission to Display Error Message. Gracefull fail.."); }
         }
     }
 }
